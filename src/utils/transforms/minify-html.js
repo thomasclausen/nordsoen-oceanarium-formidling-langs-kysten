d828d0724941e6e8a1840313199b60a94921f873
Thomas Clausen <kontakt@thomasclausen.dk>
2021-10-01 14:19:36 +0200

    .

---
+++ src/utils/transforms/minify-html.js
@@ -0,0 +1,17 @@
+console.log('MINIFY: html');
+
+const htmlmin = require('html-minifier');
+
+module.exports = (content, outputPath) => {
+  if (outputPath.endsWith('.html')) {
+    let minified = htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true
+    });
+
+    return minified;
+  }
+
+  return content;
+}
